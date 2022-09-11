81d6bb21f7d9acc413d067e612959d45e541ef35
OSABHIE FERGUSON <ebenezerosas92@gmail.com>
2022-09-10 21:51:09 -0400

    services

--- src/Pages/Home/services/Services.js
+++ src/Pages/Home/services/Services.js
@@ -1,18 +1,38 @@
 import React from "react";
 import ServicesStyle from "./ServicesStyle";
 import Carousel from "../../../Components/Carousel/Carousel";
-import architectureImg from "../../../Assets/architectureImg.jpeg";
+import architectureImg from "../../../Assets/Services/architectureImg.jpeg";
+import civilImg from "../../../Assets/Services/civil.png";
+import surveyImg from "../../../Assets/Services/survey.png";
+import realEstateImg from "../../../Assets/Services/realEstate.png";
+import generalSupplyImg from "../../../Assets/Services/generalSupply.png";
 
 const Services = () => {
-  const archTitle = "Architecture";
   const archBody =
     "Our Portfolio Is Comprised of Home Plans from Designers and Architects. Search our collection of 30k house plans by 200 designers.";
 
   const carouselObj = [
-    { cardImage: architectureImg, cardTitle: archTitle, cardBody: archBody },
-    { cardImage: architectureImg, cardTitle: archTitle, cardBody: archBody },
-    { cardImage: architectureImg, cardTitle: archTitle, cardBody: archBody },
-    { cardImage: architectureImg, cardTitle: archTitle, cardBody: archBody },
+    {
+      cardImage: architectureImg,
+      cardTitle: "Architecture",
+      cardBody: archBody,
+    },
+    {
+      cardImage: civilImg,
+      cardTitle: "Engineering",
+      cardBody: archBody,
+    },
+    { cardImage: surveyImg, cardTitle: "Surveying", cardBody: archBody },
+    {
+      cardImage: realEstateImg,
+      cardTitle: "Real Estate",
+      cardBody: archBody,
+    },
+    {
+      cardImage: generalSupplyImg,
+      cardTitle: "General Supplies",
+      cardBody: archBody,
+    },
   ];
   return (
     <ServicesStyle>
